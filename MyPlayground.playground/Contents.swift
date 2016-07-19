bb6f52ed30f8df589e1eed435c58b4f53aced40c
checkmyfunk <vitali.potchekin@gmail.com>
2016-07-19 01:46:23 -0400

    playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,8 +2,15 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let individualScores = [43, 39, 89, 92, 97, 44, 65, 32, 49, 74]
+var teamScore = 0
 
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
 
-print(str)
-
+print(teamScore)
